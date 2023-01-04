e0a801123628cbe3a7a4c6fc8521e652993352aa
Daniil-Sokolskiy <danil_sokolskiy200215@mail.ru>
2023-01-04 21:01:17 +0400

    add filling array

--- Program.cs
+++ Program.cs
@@ -1,1 +1,27 @@
-﻿
+﻿    string[] AS; // ссылка на массив строк
+    int count; // текущее количество строк в массиве
+    string s;
+    string[] AS2; // дополнительная переменная-ссылка - сохраняет старый массив строк
+
+    Console.WriteLine("Enter strings:");
+
+    count = 0; // обнулить количество строк
+    AS = new string[count]; // выделить память для 0 строк
+
+    do
+    {
+        s = Console.ReadLine();
+        if (s != "")
+        {
+            count++;
+            AS2 = new string[count];
+            for (int i = 0; i < AS2.Length - 1; i++)
+                AS2[i] = AS[i];
+            AS2[count - 1] = s;
+            AS = AS2;
+        }
+    } while (s != "");
+
+    for (int i = 0; i < AS.Length; i++)
+        Console.WriteLine("AS[{0}] = {1}", i, AS[i]);
+
