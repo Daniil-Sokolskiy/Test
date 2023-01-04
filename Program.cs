1ae80cec8144d9dc7a398578d8690514f56dc32c
Daniil-Sokolskiy <danil_sokolskiy200215@mail.ru>
2023-01-04 21:18:19 +0400

    add filling array update

--- Program.cs
+++ Program.cs
@@ -1,27 +1,28 @@
-﻿    string[] AS; // ссылка на массив строк
-    int count; // текущее количество строк в массиве
-    string s;
-    string[] AS2; // дополнительная переменная-ссылка - сохраняет старый массив строк
+﻿string[] AS; // ссылка на массив строк
+int count; // текущее количество строк в массиве
+string s;
+string[] AS2; // дополнительная переменная-ссылка - сохраняет старый массив строк
 
-    Console.WriteLine("Enter strings:");
+Console.WriteLine("Enter strings:");
 
-    count = 0; // обнулить количество строк
-    AS = new string[count]; // выделить память для 0 строк
+count = 0; // обнулить количество строк
+AS = new string[count]; // выделить память для 0 строк
 
-    do
+do
+{
+    s = Console.ReadLine();
+    if (s != "")
     {
-        s = Console.ReadLine();
-        if (s != "")
-        {
-            count++;
-            AS2 = new string[count];
-            for (int i = 0; i < AS2.Length - 1; i++)
-                AS2[i] = AS[i];
-            AS2[count - 1] = s;
-            AS = AS2;
-        }
-    } while (s != "");
-
-    for (int i = 0; i < AS.Length; i++)
-        Console.WriteLine("AS[{0}] = {1}", i, AS[i]);
+        count++;
+        AS2 = new string[count];
+        for (int i = 0; i < AS2.Length - 1; i++)
+            AS2[i] = AS[i];
+        AS2[count - 1] = s;
+        AS = AS2;
+    }
+} while (s != "");
+Console.Write("AS = [");
+for (int i = 0; i < AS.Length; i++)
+    Console.Write($"'{AS[i]}'");
+Console.WriteLine("]");
 
